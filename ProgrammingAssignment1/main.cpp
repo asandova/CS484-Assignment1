448eb88badcf8e7bc9bdf027d6131cd4aad7f035
asandova <asandova@nmsu.edu>
2018-09-11 12:28:19 -0600

    init

---
+++ ProgrammingAssignment1/main.cpp
@@ -0,0 +1,19 @@
+/*
+	File: main.cpp
+	Author: August B. Sandoval
+	Class: CS484
+	Date: 2018-9-11
+
+	Purpose:
+		This is the main file for the CS484 programming assignment.
+		This assignment is to implementent our own Large Number class for computation
+		This assignment will also support Factorial and Binomial formulas that work natively with the custom Large Number class
+*/
+
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	return 0;
+}
