04f0fbbc932ac8832c18be868babfcd90357e1c4
asandova <asandova@nmsu.edu>
2018-09-17 14:20:10 -0600

    9/17/2018 Changes Added Files Proability.cpp and Proability.h Added Factorals and binomial functions with LargeNum Not tested yet

---
+++ ProgrammingAssignment1/Proability.cpp
@@ -0,0 +1,68 @@
+
+#include "Proability.h"
+#include "LargeNum.h"
+#include <vector>
+
+using namespace std;
+
+vector<LargeNum> Factorials = vector<LargeNum>();
+
+LargeNum factorial(int n) {
+	if (Factorials.empty()) {
+		Factorials.push_back(LargeNum(1));
+	}
+
+
+	if (n == 0) {
+		return Factorials[0];
+	}
+	else if (n < Factorials.size()) {
+		return Factorials[n - 1];
+	}
+	else {
+		expandFactorial(n);
+		return Factorials.back();
+	}
+
+}
+
+void expandFactorial(int n) {
+	if (n < Factorials.size()) {
+		return;
+	}
+	else {
+		int dif = n - Factorials.size();
+		int current = Factorials.size();
+		for (int i = 0; i < dif; i++, current++) {
+			LargeNum temp = Factorials[current - 2] * current;
+			Factorials.push_back(temp);
+		}
+
+	}
+}
+
+LargeNum Binomial(int n, int x, float p) {
+	LargeNum result;
+	LargeNum trials = LargeNum(n);
+	LargeNum successes = LargeNum(x);
+	LargeNum proability = LargeNum(p);
+	//Do something
+
+	LargeNum fact, success, fail;
+
+	fact = factorial(n) / (factorial(n - x)*factorial(x));
+	success = LargeNum::pow(successes, x);
+	fail = (LargeNum(1) - successes);
+	fail = LargeNum::pow(fail, n - x);
+
+	return result;
+}
+
+LargeNum BinomialSum(int n, int x, float p) {
+	LargeNum result = Binomial(n,0,p);
+	//Do something
+	for (int i = 1; i < x; i++) {
+		result = result + Binomial(n, i, p);
+	}
+	return result;
+}
---
+++ ProgrammingAssignment1/Proability.h
@@ -0,0 +1,16 @@
+#ifndef PROABILITY_H
+#define PROABILITY_H
+
+#include "LargeNum.h"
+#include <vector>
+
+std::vector<LargeNum> Factorials;
+
+LargeNum factorial(int);
+void expandFactorial(int n);
+
+LargeNum Binomial(int n, int x, float p);
+
+LargeNum BinomialSum(int n, int x, float p);
+
+#endif // !PROABILITY_H
